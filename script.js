26729d2233bd48c064b031e251d0a54615ae6d64
Harika Aleti <haleti@primesoft.net>
2026-03-18 23:22:35 +0530

    Add add-to-cart functionality with dynamic cart count

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const products = [
-    {name:"Shoes", price:1000},
-    {name:"Watches", price:3000}
+    {id: 1,name:"Shoes", price:1000},
+    {id: 2,name:"Bags", price:2000},
+    {id: 3,name:"Watches", price:3000}
 ];
+
+let cart=[];
 
 const container = document.getElementById("product-container");
 
@@ -11,7 +14,21 @@ products.forEach(function(item)
         <div class="card">
             <h2>${item.name}</h2>
             <p>Price: ₹${item.price}</p>
+            <button onclick="addToCart(${item.id})"> Add to Cart    </button>
         </div>
     `;
     container.innerHTML += card;
 });
+
+function addToCart(id) {
+    const product = products.find(function(item){
+        return item.id === id;
+    });
+    cart.push(product);
+    updateCartCount();
+}
+
+function updateCartCount() {
+    document.getElementById("cart-count").textContent = cart.length;
+}
+
